6ab9f144065aa4391efecb3fb395be88d4f6fa32
Malanius <malanius@seznam.cz>
2014-06-16 22:23:07 +0200

    Non-letter characters are not longer cyphered.

--- src/caesarencryption/CaesarEncryption.java
+++ src/caesarencryption/CaesarEncryption.java
@@ -15,12 +15,23 @@ public class CaesarEncryption {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO user defined input
-        String input = "Ahoj.";
+        // TODO user defined input and offset
+        String input = "Ahoj!";
         String encrypted = "";
+        int min = (int) 'A';
+        int max = (int) 'z';
+        System.out.printf("Min: %d, Max: %d. \n", min, max);
         int offset = 1;
         for (char c : input.toCharArray()) { //Start iterating the input.
-            encrypted = encrypted + (char)((int)c + offset); //Saves a off  seted characer at each cycle
+            //encrypted = encrypted + (char)((int)c + offset); //Saves a off  seted characer at each cycle
+            int chr = (int) c;
+            if (chr >= min && chr <= max) {
+                encrypted = encrypted + (char)(chr + offset);
+            }
+            else
+            {
+                encrypted = encrypted + c;
+            }
         } //End of iteration
         System.out.println("Encrypted sentence is: " + encrypted); //Prints the result
     } //End of main
