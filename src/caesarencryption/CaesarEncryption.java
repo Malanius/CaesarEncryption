af265e111f4b0f7f17fbddf1d716c959071ad3b1
Malanius <malanius@seznam.cz>
2014-06-16 19:41:08 +0200

    Added the basic logic.

--- src/caesarencryption/CaesarEncryption.java
+++ src/caesarencryption/CaesarEncryption.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package caesarencryption;
 
 /**
@@ -16,7 +15,14 @@ public class CaesarEncryption {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+        // TODO user defined input
+        String input = "Ahoj.";
+        String encrypted = "";
+        int offset = 1;
+        for (char c : input.toCharArray()) { //Start iterating the input.
+            encrypted = encrypted + (char)((int)c + offset); //Saves a off  seted characer at each cycle
+        } //End of iteration
+        System.out.println("Encrypted sentence is: " + encrypted); //Prints the result
+    } //End of main
+
+} //End of class
