c1be802697e5941f51c148abbf77bb8edcdbf615
Malanius <malanius@seznam.cz>
2014-06-17 17:32:10 +0200

    Tweaked the valid range exception

--- src/caesarencryption/CaesarEncryption.java
+++ src/caesarencryption/CaesarEncryption.java
@@ -30,20 +30,17 @@ public class CaesarEncryption {
         Scanner sc = new Scanner(System.in); //Seting up scanner for user input
         System.out.print("Insert the sentence to be encrypted:\n→");
         sentence = sc.nextLine(); //User defined input sentence
-        // TODO user defined offset
         //User defined offset
-        boolean looped = true;
-        while (looped) { //Start of correct input loop
+        while (true) { //Start of correct input loop
             System.out.print("Specify required offset. Valid range is -25 to +25.\n→");
             input = sc.nextLine();
             try { //Try block for nput validity
                 offset = Integer.parseInt(input); //Try to parse the input
-                if (offset >= -25 && offset <= 25){ //Check if the input is in range (this won't proceed if input is not integer
+                if (offset >= -25 && offset <= 25) { //Check if the input is in range (this won't proceed if input is not integer
                     break; //If it is, break the check loop and continue
-                }
-                else //If out of range
+                } else //If out of range
                 {
-                    offset = Integer.parseInt("induced error"); //Induce an error
+                    throw new NumberFormatException(); //Induce an error
                 }
             } catch (NumberFormatException e) { //If input is invalid
                 System.err.println(input + " is not a valid number!"); //Inform the user that he can't do what I need from him
@@ -68,5 +65,5 @@ public class CaesarEncryption {
 
         System.out.println(
                 "Encrypted sentence is: " + encrypted); //Prints the result
-    }
+    } //End of main
 } //End of class
